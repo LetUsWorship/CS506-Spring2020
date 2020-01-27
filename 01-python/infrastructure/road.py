e173f520a58087e2c28ef84048c0dc0b900f11cd
Xiaochen Xue <xxc@bu.edu>
2020-01-27 17:39:14 -0500

    adding edited tree file to enhance holiday spirit along with the example road file we did last class.

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,28 @@
-def draw_road():
-    print("road not found")
+def draw_road(length=10,axis = "horizontal"):
+    
+    VALID = ["horizontal","vertical"]
+    
+    if axis not in VALID:
+        raise ValueError("incorrect axis")
+
+    if length < 0:
+        raise ValueError("incorrect length")
+    
+    if axis == "horizontal":
+        return draw_H(length)
+
+    else:
+        return draw_V(length)
+
+    return
+
+def draw_H(length):
+    for i in range(length):
+        print("=", end ="")
+    print()
+    return
+
+def draw_V(length):
+    for i in range(length):
+        print("||")
     return
--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,30 @@
-def draw_tree():
-    print("tree not found")
+#the parameter in this function determines how big the tree is going to be
+#the return values will be a christmas tree drawn by symbols
+def draw_tree(size=3):
+
+    if size <= 0:
+        return
+        
+    Triangle = ""
+    Triangle2 = ""
+    Triangle3 = ""
+    for i in range (size):
+        Triangle += ("*"*((i*4)+1)).center((((size+4)*4))," ")
+        Triangle += "\n"
+    Triangle += ("|"*((i*4)+3)).center((((size+4)*4))," ")
+    print(Triangle)
+    
+    for i in range (1,size+2):
+        Triangle2 += ("+"*((i*4)+1)).center((((size+4)*4))," ")
+        Triangle2 += "\n"
+    Triangle2 += ("|"*((i*4)+3)).center((((size+4)*4))," ")
+    print(Triangle2)
+
+    for i in range (2,size+4):
+        Triangle3 += (":"*((i*4)+1)).center((((size+4)*4))," ")
+        Triangle3 += "\n"
+    print(Triangle3)
+
+    
     return
+    
